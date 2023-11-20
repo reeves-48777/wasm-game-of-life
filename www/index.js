71dbf4c0002c73a1385cae8d7c7db5c33e6b2ca9
Thomas Durand <durandthomas@protonmail.com>
2023-11-21 00:14:21 +0100

    feat - add fps count

--- www/index.js
+++ www/index.js
@@ -15,6 +15,47 @@ universe.random_cells();
 const width = universe.width();
 const height = universe.height();
 
+const fps = new class {
+    constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimeStamp = performance.now();
+    }
+
+    render() {
+        // convert delta time since the last frame render into a measure of fps
+        const now = performance.now();
+        const delta = now -this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const fps = 1 / delta * 1000;
+
+        // save only the latest 100 timings
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        // find the max, min, average of the timings
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+        let avg = sum / this.frames.length;
+
+        this.fps.textContent = `
+        Frames per Second:
+    latest = ${Math.round(fps)}
+    avg = ${Math.round(avg)}
+    min = ${Math.round(min)}
+    max = ${Math.round(max)}
+        `.trim();
+    }
+}
+
 // On init le canvas pour qu'il ait la place pour toutes les cellules et un bord de 1px;
 const canvas = document.getElementById("game-of-life-canvas");
 canvas.height = (CELL_SIZE + 1) * height + 1;
@@ -213,6 +254,7 @@ if (canvas.getContext) {
     let animationId = null;
     const renderLoop = () => {
         // debugger;
+        fps.render();
         for (let i = 0; i < ticksPerFrame.value; i++) {
             universe.tick();
         }
